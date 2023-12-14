b9e0421f1f159a1a95808e89ee4546a8ac445c1a
Naim <mdnaim01303202218@gmail.com>
2023-12-14 18:08:41 +0600

    video add

--- Foody Zone/src/container/Intro/Intro.jsx
+++ Foody Zone/src/container/Intro/Intro.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import "./Intro.css";
+import { meal } from "../../constants";
 
-import './Intro.css';
+function Intro() {
+  const [palyvideo, setPlayvideo] = useState(false);
+  const videRef = React.useRef();
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+  const handleVideo = () => {
+    setPlayvideo((prevPlayVideo) => !prevPlayVideo);
+    if (palyvideo) {
+      videRef.current.pause();
+    } else {
+      videRef.current.play();
+    }
+  };
+  return (
+    <div className="app__video">
+      <video
+        src={meal}
+        ref={videRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className="app__video-overlay flex__center">
+        <div
+          className="app__video-overlay_circle flex__center"
+          onClick={handleVideo}
+        >
+          {palyvideo ? <BsFillPauseFill color="#fff" fontSize={30} />  : <BsFillPlayFill color="#fff" fontSize={30} /> }
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Intro;
