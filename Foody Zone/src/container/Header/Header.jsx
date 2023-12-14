192f647a4c58a510d5ce6b4c2f4d22100b1c9d83
Naim <mdnaim01303202218@gmail.com>
2023-12-14 16:21:44 +0600

    done

--- Foody Zone/src/container/Header/Header.jsx
+++ Foody Zone/src/container/Header/Header.jsx
@@ -11,9 +11,9 @@ const Header = () => (
         <SubHeading title="Chase the new flavour"/>
         <h1 className="app__header-h1">The key to Fine Dining</h1>
         <p className="p__opensans" style={{margin: '2rem 0'}}>Experience Delicious Meals and Exceptional Service at Our Restaurant. Every Dish is Crafted to Perfection for a Memorable Dining Experience.</p>
-        <button className="custom__button">Explore Menu</button>
+        <button className="custom__button">Explore Menu</button> 
       </div>
-
+{/* naim */}
       <div className="app__wrapper_img">
         <img src={images.welcome} alt="header img" />
       </div>
